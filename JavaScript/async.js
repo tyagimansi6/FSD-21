4ba256c6d2303c4f15c12fe78fe2c0145a95e70d
MANSI TYAGI <archanatyagi209@gmail.com>
2026-08-25 09:38:22 +0530

    async and sync in js

---
+++ JavaScript/async.js
@@ -0,0 +1,10 @@
+console.log("task 1");
+console.log("task 2");
+setTimeout(()=>{
+    console.log("task 3");
+}, 200);
+setTimeout(()=>{
+    console.log("task 6");
+}, 20);
+console.log("task 4");
+console.log("task 5");
---
+++ JavaScript/sync.js
@@ -0,0 +1,8 @@
+console.log("task 1");
+for(let i=1; i<=100000;i++)
+{
+    console.log(i);
+}
+console.log("task 2");
+console.log("task 3");
+console.log("hellooooo abes");
